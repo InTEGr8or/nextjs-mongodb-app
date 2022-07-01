25fd32ef763d80d53999e793b777cafbcfe23964
alexstouffer <bizkite@hotmail.com>
2022-06-30 18:44:15 -0700

    TODO: Create layout component for full width BG

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -18,7 +18,9 @@ const Card = forwardRef(function Card(
         className={styles.card}
         ref={ref}
     >
-        <Image src={img} alt={alt} width="150" height="150" className="icon" />
+        <div  className={styles.icon}>
+            <Image src={img} alt={alt} width="150" height="150" />
+        </div>
         <div className={styles.container}>
             <h4><b>{title}</b></h4> 
             <p>{children}</p>
--- page-components/Index/Hero.jsx
+++ page-components/Index/Hero.jsx
@@ -7,44 +7,50 @@ import styles from './Hero.module.css';
 
 const Hero = () => {
   return (
-    <Wrapper>
-      <div>
-        <h1 className={styles.title}>
-          <span className={styles.project}>Sweet Sigs</span>
-          <span className={styles.signature}>Email Signature</span>
-          <span>Generator</span>
-        </h1>
-        <Container justifyContent="center" className={styles.buttons}>
-          <Container>
-            <Link passHref href="/feed">
-              <ButtonLink className={styles.button}>Explore Feed</ButtonLink>
-            </Link>
+    <>
+      <Wrapper>
+        <div>
+          <h1 className={styles.title}>
+            <span className={styles.project}>Sweet Sigs</span>
+            <span className={styles.signature}>Email Signature</span>
+            <span>Generator</span>
+          </h1>
+          <Container justifyContent="center" className={styles.buttons}>
+            <Container>
+              <Link passHref href="/feed">
+                <ButtonLink className={styles.button}>Explore Feed</ButtonLink>
+              </Link>
+            </Container>
+            <Spacer axis="horizontal" size={1} />
+            <Container>
+              <ButtonLink
+                href="https://github.com/hoangvvo/nextjs-mongodb-app"
+                type="secondary"
+                className={styles.button}
+              >
+                GitHub
+              </ButtonLink>
+            </Container>
           </Container>
-          <Spacer axis="horizontal" size={1} />
-          <Container>
-            <ButtonLink
-              href="https://github.com/hoangvvo/nextjs-mongodb-app"
-              type="secondary"
-              className={styles.button}
-            >
-              GitHub
-            </ButtonLink>
+          <h3 className={styles.subtitle}>Professional Email Presentation in 3 Easy Steps!</h3>
+          <Container justifyContent="space-evenly">
+            <Card img="/images/handshake-simple-solid.svg" alt="handshake" title="Step 1:">
+              Select the size of your organization and how many signatures you need.
+            </Card>
+            <Card img="/images/address-book-solid.svg" alt="address book" title="Step 2:">
+              Choose a template and upload a spreadsheet of individuals and their contact info. 
+            </Card>
+            <Card img="/images/people-group-solid.svg" alt="group of people" title="Step 3:">
+              Generate templates to a local folder or send them directly to everyone via email.
+            </Card>
           </Container>
-        </Container>
-        <h3 className={styles.subtitle}>Professional Email Presentation in 3 Easy Steps!</h3>
-        <Container justifyContent="space-evenly">
-          <Card img="/images/handshake-simple-solid.svg" alt="handshake" title="Step 1:">
-            Tell us the size of your organization and how many signatures you need.
-          </Card>
-          <Card img="/images/address-book-solid.svg" alt="address book" title="Step 2:">
-            Choose a template and upload a spreadsheet of individuals and their contact info. 
-          </Card>
-          <Card img="/images/people-group-solid.svg" alt="group of people" title="Step 3:">
-            Generate templates to a local folder or send them directly to everyone via email.
-          </Card>
-        </Container>
+        </div>
+      </Wrapper>
+      <div className={styles.fullwidth}>
+        <h3 className={styles.subtitle}>Keep Image Appearance Consistent.</h3>
+        <p className="p">We use AWS to host your images and keep the generative process running smoothly.</p>
       </div>
-    </Wrapper>
+    </>
   );
 };
 
